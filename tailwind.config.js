47eea87433d960b7ac626f4d8cf0e80167e7567f
Ben Smith <bsmith20681@gmail.com>
2022-08-27 12:56:42 -0400

    second commit

---
+++ tailwind.config.js
@@ -0,0 +1,18 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["public/*.{html,js}"],
+  theme: {
+    container: {
+      center: true,
+    },
+    extend: {
+      fontFamily: {
+        sans: ["Work Sans"],
+      },
+      colors: {
+        "custom-black": "#181818",
+      },
+    },
+  },
+  plugins: [],
+};
